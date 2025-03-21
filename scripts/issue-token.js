d96a712e376b6d59c826e3d2117237155eaf5264
el-noir <mudasirshah9777@gmail.com>
2025-03-21 08:32:10 +0500

    script for token issuance

--- scripts/issue-token.js
+++ scripts/issue-token.js
@@ -1,9 +1,11 @@
 const TokenFarm = artifacts.require('TokenFarm')
 
-module.exports = async function(callback) {
-  let tokenFarm = await TokenFarm.deployed()
-  await tokenFarm.issueTokens()
-  // Code goes here...
-  console.log("Tokens issued!")
-  callback()
+module.exports = async function(callback){
+  
+  let tokenFarm = await TokenFarm.deployed();
+  await tokenFarm.issueTokens();
+
+  console.log("Tokens Issued!");
+
+  callback();
 }
